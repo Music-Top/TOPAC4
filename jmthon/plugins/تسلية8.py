4721108304df32674688d4c0fc1511fd4fa334c6
Music-Top <106431498+Music-Top@users.noreply.github.com>
2022-06-01 01:58:41 +0300

    Update تسلية8.py

--- jmthon/plugins/تسلية8.py
+++ jmthon/plugins/تسلية8.py
@@ -8,7 +8,7 @@ import textwrap
 from PIL import Image, ImageDraw, ImageFont
 from telethon.tl.types import InputMessagesFilterDocument
 
-from sbb_b import sbb_b
+from jmthon import jmthon
 
 from ..core.managers import edit_or_reply
 from ..helpers.functions import deEmojify, hide_inlinebot, waifutxt
